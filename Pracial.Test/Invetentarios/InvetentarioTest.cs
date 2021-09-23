7e4dcf3f3e2dc7c1ba66358f395e66ed5d18dd0d
micheelRojas <56304710+micheelRojas@users.noreply.github.com>
2021-09-23 10:57:55 -0500

    Prueba PuedoRegistrarProductosdeEntrada en Verde

--- Pracial.Test/Invetentarios/InvetentarioTest.cs
+++ Pracial.Test/Invetentarios/InvetentarioTest.cs
@@ -23,15 +23,17 @@ namespace Pracial.Test.Invetentarios
         {
 
             #region DADO EL RESTAURANTE TIENE PRODUCTO DE GASEODA DE LITRO CON UN PRECIO DE 5000 Y UN COSTO 2000 Y NO NECESITA PREPARACION
-            var producto = new Producto(nombre: "Gaseosa", costo: 2000, precio: 5000, ventaDirecta:false);
+            var producto = new Producto(nombre: "Gaseosa", costo: 2000, precio: 5000, ventaDirecta: false);
             #endregion
             #region CUANDO registre 3 gaseosa
             int cantidad = 3;
             var inventario = new Invetentario();
-            string respuesta = inventario.EntradaProductos(producto: producto, cantidad:cantidad);
+            List<Invetentario> invetentarios= new List<Invetentario>();
+            invetentarios.Add(inventario);
+            string respuesta = inventario.EntradaProductos(producto: producto, cantidad:cantidad, inventario: invetentarios);
             #endregion
             #region ENTONCES  el sistema registrara el producto en el inventario y adicionara la cantidad del mismo 
-            Assert.AreEqual(6000, inventario.valor);
+            Assert.AreEqual(6000, inventario.Valor);
             Assert.AreEqual("Su Nueva cantidad de Gaseosa es de 3", respuesta);
             #endregion
 
@@ -40,31 +42,55 @@ namespace Pracial.Test.Invetentarios
 
     internal class Invetentario
     {
-        internal double valor;
-
+        public decimal Valor { get; private set; }
+        public int Cantidad { get; private set; }
+        public Producto Producto { get; private set; }
         public Invetentario()
         {
         }
 
-        internal string EntradaProductos(Producto producto, int cantidad)
+        internal string EntradaProductos(Producto producto, int cantidad, List<Invetentario> inventario)
         {
+           
+            if (cantidad >= 0) {
+                if (ExisteProducto(inventario, producto))
+                {  
+                }
+                else {
+                    Producto = producto;
+                }
+                Cantidad += cantidad;
+                Valor = producto.Costo * Cantidad;
+
+                return $"Su Nueva cantidad de {Producto.Nombre} es de {Cantidad}";
+            }
             throw new NotImplementedException();
         }
+
+        internal bool ExisteProducto(List<Invetentario> inventario, Producto producto) {
+            if (inventario.FirstOrDefault(t => t.Producto == producto) != null) {
+                return true;
+            }
+            return false;
+
+
+        }
+       
     }
 
     internal class Producto
     {
-        private string nombre;
-        private int costo;
-        private int precio;
-        private bool ventaDirecta;
+        public string Nombre { get; private set; }
+        public int Costo { get; private set; }
+        public int Precio { get; private set; }
+        public bool VentaDirecta { get; private set; }
 
         public Producto(string nombre, int costo, int precio, bool ventaDirecta)
         {
-            this.nombre = nombre;
-            this.costo = costo;
-            this.precio = precio;
-            this.ventaDirecta = ventaDirecta;
+            Nombre = nombre;
+            Costo = costo;
+            Precio = precio;
+            VentaDirecta = ventaDirecta;
         }
     }
 }
